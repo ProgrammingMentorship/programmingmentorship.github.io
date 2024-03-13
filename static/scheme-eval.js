cf3a8ab4bf0afbcf6871d638df314a4e2ffbb629
Ryan Fleck <Ryan.Fleck@protonmail.com>
2024-03-13 17:32:36 -0600

    Reformat and add comments to js file

--- static/scheme-eval.js
+++ static/scheme-eval.js
@@ -6,6 +6,7 @@ function scmEval(e) {
 
   console.log(e);
 
+  // Collect elements related to the "Run Program" button.
   var out = e + "-out";
   var output = document.getElementById(out);
 
@@ -16,6 +17,7 @@ function scmEval(e) {
   var content = document.getElementById(e).value;
   console.log(content);
 
+  // Enables textarea and button once evaluation is finished.
   function enable(){
     setTimeout(function(){
       button.innerHTML = "Run Program"
@@ -24,6 +26,7 @@ function scmEval(e) {
     }, 200);
   }
 
+  // Disables the button and textarea while stuff is evaluating
   var disablePromise = new Promise(function(resolve, reject){
     // Set things.
     button.innerHTML = "Running...";
@@ -32,35 +35,35 @@ function scmEval(e) {
     setTimeout(function(){resolve("done");}, 10);
   });
   
+  // Kick off short time delay, eval, and longer delay
+  // so the students can percieve that the execution happened.
   disablePromise
     .then(function(){
       return lips.exec(content);
     })
     .then(
-     function(results) {
-       output.innerHTML = "";
-       results.forEach(function(result) {
-         if(result){
-           console.log(result.toString());
-           var node = document.createElement("p");
-           node.classList.add("scm-success");
-           node.innerHTML = result.toString();
-           output.appendChild(node);
-         }
-         }
-       );
-     }
-  ).catch (function(error) {
-    output.innerHTML = "";
-    console.log("Issue encountered while interpreting.");
-    console.log(error.message);
-           var node = document.createElement("p");
-           node.innerHTML = error.message.toString();
-           node.classList.add("scm-error");
-           output.appendChild(node);
-
-    console.error(error);
+      function(results) {
+        output.innerHTML = "";
+        results.forEach(function(result) {
+          if(result){
+            console.log(result.toString());
+            var node = document.createElement("p");
+            node.classList.add("scm-success");
+            node.innerHTML = result.toString();
+            output.appendChild(node);
+          }
+        });
+      }
+    ).catch (function(error) {
+      output.innerHTML = "";
+      console.log("Issue encountered while interpreting.");
+      console.log(error.message);
+      var node = document.createElement("p");
+      node.innerHTML = error.message.toString();
+      node.classList.add("scm-error");
+      output.appendChild(node);
+      console.error(error);
     }).finally(function(){
-    enable();
-  });
+      enable();
+    });
 }
